8adc0d6830a05eb083ee6ed5f3e620820d89355e
AlexGibernau <124159288+AlexGibernau@users.noreply.github.com>
2024-04-11 17:24:06 +0200

    preparacion

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.agc.proyecto_m06_m09 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.agc.proyecto_m06_m09 to javafx.fxml;
+    exports org.agc.proyecto_m06_m09;
+}
